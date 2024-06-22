acf9efb45b12e25db25b447c4328a27851fe3f9f
Nayeem Laheji <132880575+nayeem003@users.noreply.github.com>
2024-06-22 10:24:56 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// int main() {
+//    cout<<"Enter your name: ";
+//    string name;
+//    cin>>name;
+//    cout<<"Hello Mr. Beast "<<name;
+//}
+
+//add 2 number
+int add(int a, int b);
+
+
+
+
+int main(){
+    int a;
+    cout<<"Enter the value of a " <<endl;
+
+    cin >>a;
+
+    int b;
+    cout<<"Enter the value of a " <<endl;
+    cin >>b;
+
+    int add(a,b);
+
+    cout<<"Addition of two numbers is : "<< add << endl;
+
+
+    int result=a+b;
+return result;
+
+    
+}
+
+int add(int a, int b)
+{
+return 0;
+}
